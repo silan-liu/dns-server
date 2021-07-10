03e4596a9fc77f22302d9524e8340492b284358b
liusilan2 <liusilan2@yy.com>
2021-07-10 18:34:43 +0800

    delete pods

--- DNSServerNew/Pods/Target Support Files/Pods-DNSServerNew-DNSServerNewUITests/Pods-DNSServerNew-DNSServerNewUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DNSServerNew_DNSServerNewUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DNSServerNew_DNSServerNewUITests
-@end
--- DNSServerNew/Pods/Target Support Files/Pods-DNSServerNew/Pods-DNSServerNew-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DNSServerNew : NSObject
-@end
-@implementation PodsDummy_Pods_DNSServerNew
-@end
